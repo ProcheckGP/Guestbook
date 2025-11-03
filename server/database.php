1d8abd21fb85b4971ba77feda9506e5ab81e20bb
ProcheckGP <andrey.39.con@gmail.com>
2025-11-03 19:39:09 +0300

    add connection to bd

---
+++ server/database.php
@@ -0,0 +1,48 @@
+<?php
+class Database
+{
+    private static $instance = null;
+    private $connection = null;
+
+    protected function __construct()
+    {
+        $config = require __DIR__ . '/config.php';
+
+        $this->connection = new PDO(
+            $config['dsm'],
+            $config['username'],
+            $config['password'],
+            $config['options']
+        );
+    }
+
+    protected function __clone() {}
+
+    public function __wakeup()
+    {
+        throw new \BadMethodCallException('Не удаётся десериализовать подключене к базе данных');
+    }
+
+    public static function getInstance(): Database
+    {
+        if (null === self::$instance) {
+            self::$instance = new static();
+        }
+        return self::$instance;
+    }
+
+    public static function connection(): \PDO
+    {
+        return static::getInstance()->connection;
+    }
+
+    public static function prepare($statement): \PDOStatement
+    {
+        return static::connection()->prepare($statement);
+    }
+
+    public static function lastInsertId()
+    {
+        return intval(static::connection()->lastInsertId());
+    }
+}
