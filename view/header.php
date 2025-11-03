e477e53244b74c60c3d63824f3cfee2fe9e62d0e
ProcheckGP <andrey.39.con@gmail.com>
2025-11-03 20:47:17 +0300

    add style to messages

--- view/header.php
+++ view/header.php
@@ -4,14 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GuestBook</title>
-    <link rel="stylesheet" href="resources/bootstrap.min.css">
-    <link rel="stylesheet" href="resources/mainStyle.css">
+    <title>Гостевая книга</title>
+    <link rel="stylesheet" href="resources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="resources/css/mainStyle.css">
 </head>
 
 <body>
     <header>
-        <div class="container text-center">
+        <div class="container text-center page-title">
             <h1>ОСТАВЬТЕ СВОЁ СООБЩЕНИЕ НИЖЕ</h1>
         </div>
     </header>
--- view/outputMessage.php
+++ view/outputMessage.php
@@ -5,12 +5,22 @@
 <?php else: ?>
     <div>
         <?php foreach ($messages as $message): ?>
-            <div>
-                <?php echo htmlspecialchars($message['name']); ?>
-                <?php echo htmlspecialchars($message['email']); ?>
-                <?php echo htmlspecialchars($message['title']); ?>
-                <?php echo htmlspecialchars($message['content']); ?>
-                <?php echo htmlspecialchars($message['created']); ?>
+            <div class="container form-output-message">
+                <div class="row form-output-message-header">
+                    <div class="col-6">
+                        <span class="form-output-message-name"><?php echo htmlspecialchars($message['name']); ?></span>
+                        <span class="form-output-message-email"><?php echo htmlspecialchars($message['email']); ?></span>
+                    </div>
+                    <div class="col-6 form-output-message-time align-left">
+                        <?php echo htmlspecialchars($message['created']); ?>
+                    </div>
+                </div>
+                <div>
+                    <span>Тема: "<?php echo htmlspecialchars($message['title']); ?>"</span>
+                </div>
+                <div>
+                    <?php echo htmlspecialchars($message['content']); ?>
+                </div>
             </div>
         <?php endforeach; ?>
     </div>
